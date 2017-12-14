f03345c775c45362cf255cd8c8b55e44f02ea5e3
arditsarja <arditsarja@gmail.com>
2017-12-14 12:16:23 +0100

    FirstCommit

---
+++ app/src/main/java/com/pubnub/example/android/datastream/pubnubdatastreams/Constants.java
@@ -0,0 +1,15 @@
+package com.pubnub.example.android.datastream.pubnubdatastreams;
+
+public class Constants {
+    public static final String PUBNUB_PUBLISH_KEY = "pub-c-8e169ab9-f772-4824-a595-b076e521a5c4";     // Replace with your publish key
+//    public static final String PUBNUB_PUBLISH_KEY = "pub-c-450b5ec5-cd1d-40af-bd33-eded45cc0411";     // Replace with your publish key
+    public static final String PUBNUB_SUBSCRIBE_KEY = "sub-c-b5883622-8334-11e7-9bef-b2d410151acd";   // Replace with your subscribe key
+//    public static final String PUBNUB_SUBSCRIBE_KEY = "sub-c-77622dca-d9d9-11e7-9683-56158441f777";   // Replace with your subscribe key
+
+    public static final String CHANNEL_NAME = "0xABABABAB,0xCBCBCBCB";     // replace with more meaningful channel name
+//    public static final String MULTI_CHANNEL_NAMES = "0xCDCDCDCD,0xEFEFEFEF,0xGHGHGHGH";    // ditto
+    public static final String MULTI_CHANNEL_NAMES = "0xCDCDCDCD,0xEFEFEFEF";    // ditto
+
+    public static final String DATASTREAM_PREFS = "com.pubnub.example.android.datastream.pubnubdatastreams.DATASTREAM_PREFS";
+    public static final String DATASTREAM_UUID = "com.pubnub.example.android.datastream.pubnubdatastreams.DATASTREAM_UUID";
+}
