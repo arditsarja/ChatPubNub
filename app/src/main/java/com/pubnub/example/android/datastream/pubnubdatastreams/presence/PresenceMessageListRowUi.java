7f76b69a01e1ff07dfa86288660caa04081fe0f7
arditsarja <arditsarja@gmail.com>
2017-12-19 10:41:28 +0100

    in the road

--- app/src/main/java/com/pubnub/example/android/datastream/pubnubdatastreams/presence/PresenceMessageListRowUi.java
+++
@@ -1,9 +0,0 @@
-package com.pubnub.example.android.datastream.pubnubdatastreams.presence;
-
-import android.widget.TextView;
-
-public class PresenceMessageListRowUi {
-    public TextView sender;
-    public TextView presence;
-    public TextView timestamp;
-}
