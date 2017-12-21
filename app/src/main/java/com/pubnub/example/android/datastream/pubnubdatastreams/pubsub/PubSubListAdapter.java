46ad51541a03bb601dc5c7fd78ae0c3b947f534b
arditsarja <arditsarja@gmail.com>
2017-12-21 11:35:09 +0100

    strarting changes

--- app/src/main/java/com/pubnub/example/android/datastream/pubnubdatastreams/pubsub/PubSubListAdapter.java
+++ app/src/main/java/com/pubnub/example/android/datastream/pubnubdatastreams/pubsub/PubSubListAdapter.java
@@ -2,13 +2,15 @@ package com.pubnub.example.android.datastream.pubnubdatastreams.pubsub;
 
 import android.app.Activity;
 import android.content.Context;
-import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
+import com.pubnub.example.android.datastream.pubnubdatastreams.MainActivity;
 import com.pubnub.example.android.datastream.pubnubdatastreams.MainActivity1;
 import com.pubnub.example.android.datastream.pubnubdatastreams.R;
 
@@ -29,7 +31,7 @@ public class PubSubListAdapter extends ArrayAdapter<PubSubPojo> {
     @Override
     public void add(PubSubPojo message) {
 //        this.values.add(0, message);
-        this.values.add( message);
+        this.values.add(message);
 
         ((Activity) this.context).runOnUiThread(new Runnable() {
             @Override
@@ -39,6 +41,7 @@ public class PubSubListAdapter extends ArrayAdapter<PubSubPojo> {
         });
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     public View getView(final int position, View convertView, ViewGroup parent) {
         PubSubPojo dsMsg = this.values.get(position);
@@ -58,11 +61,14 @@ public class PubSubListAdapter extends ArrayAdapter<PubSubPojo> {
             msgView = (PubSubListRowUi) convertView.getTag();
         }
 
-
-
         msgView.sender.setText(dsMsg.getSender());
         msgView.message.setText(dsMsg.getMessage());
         msgView.timestamp.setText(dsMsg.getTimestamp());
+        if (dsMsg.getSender().equals(MainActivity1.mUsername)) {
+            msgView.sender.setTextAppearance(R.style.boldText);
+            msgView.message.setTextAppearance(R.style.boldText);
+            msgView.message.setTextAppearance(R.style.boldText);
+        }
 
         return convertView;
     }
